0735ea8064e945741663b1e1db0d820a9f1ae1e9
leiyalin <leiyalin@htbaobao.com>
2020-01-14 16:59:09 +0800

    修改：登录密码加入admin的MD5

--- src/mock/services/auth.js
+++ src/mock/services/auth.js
@@ -3,8 +3,7 @@ import { builder, getBody } from '../util'
 
 const username = ['admin', 'super']
 // 强硬要求 ant.design 相同密码
-// '21232f297a57a5a743894a0e4a801fc3',
-const password = ['8914de686ab28dc22f30d3d8e107ff6c'] // admin, ant.design
+const password = ['21232f297a57a5a743894a0e4a801fc3', '8914de686ab28dc22f30d3d8e107ff6c'] // admin, ant.design的MD5加密
 
 const login = (options) => {
   const body = getBody(options)
