6bcac8240f733d95b44a4fca674fb46ed5e14e25
leiyalin <leiyalin@htbaobao.com>
2020-01-14 16:59:42 +0800

    修改：admin的用户名

--- src/mock/services/user.js
+++ src/mock/services/user.js
@@ -5,7 +5,7 @@ const info = (options) => {
   console.log('options', options)
   const userInfo = {
     'id': '4291d7da9005377ec9aec4a71ea837f',
-    'name': '天野远子',
+    'name': '小无益',
     'username': 'admin',
     'password': '',
     'avatar': '/avatar2.jpg',
