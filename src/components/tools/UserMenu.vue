db1a2168d5f1d95e29e47310d1590ce02b51da27
leiyalin <leiyalin@htbaobao.com>
2020-01-14 16:56:22 +0800

    修改：顶部菜单样式

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="user-wrapper">
     <div class="content-box">
-      <a href="https://pro.loacg.com/docs/getting-started" target="_blank">
-        <span class="action">
-          <a-icon type="question-circle-o"></a-icon>
-        </span>
-      </a>
-      <notice-icon class="action"/>
+      <!-- notice：信息通知组件 -->
+      <!-- <notice-icon class="action"/> -->
       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
           <a-avatar class="avatar" size="small" :src="avatar"/>
           <span>{{ nickname }}</span>
         </span>
         <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
-          <a-menu-item key="0">
+          <!-- <a-menu-item key="0">
             <router-link :to="{ name: 'center' }">
               <a-icon type="user"/>
               <span>个人中心</span>
@@ -24,10 +20,10 @@
               <a-icon type="setting"/>
               <span>账户设置</span>
             </router-link>
-          </a-menu-item>
+          </a-menu-item> -->
           <a-menu-item key="2" disabled>
             <a-icon type="setting"/>
-            <span>测试</span>
+            <span>修改密码</span>
           </a-menu-item>
           <a-menu-divider/>
           <a-menu-item key="3">
@@ -43,13 +39,13 @@
 </template>
 
 <script>
-import NoticeIcon from '@/components/NoticeIcon'
+// import NoticeIcon from '@/components/NoticeIcon'
 import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'UserMenu',
   components: {
-    NoticeIcon
+    // NoticeIcon
   },
   computed: {
     ...mapGetters(['nickname', 'avatar'])
@@ -72,8 +68,6 @@ export default {
               description: err.message
             })
           })
-        },
-        onCancel () {
         }
       })
     }
