4dfe4a6710049209715353d1e40de35ff2d314b0
leiyalin <leiyalin@htbaobao.com>
2020-01-14 17:00:14 +0800

    修改：if语句添加大括号

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,9 @@ import { constantRouterMap } from '@/config/router.config'
 // hack router push callback
 const originalPush = Router.prototype.push
 Router.prototype.push = function push (location, onResolve, onReject) {
-  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  if (onResolve || onReject) {
+    return originalPush.call(this, location, onResolve, onReject)
+  }
   return originalPush.call(this, location).catch(err => err)
 }
 
