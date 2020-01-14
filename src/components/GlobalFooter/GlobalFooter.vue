c28cf98b77dfe8b97c90435b96fb042c86ec85f8
leiyalin <leiyalin@htbaobao.com>
2020-01-14 16:54:36 +0800

    修改：GlobalFooter.vue 样式

--- src/components/GlobalFooter/GlobalFooter.vue
+++ src/components/GlobalFooter/GlobalFooter.vue
@@ -1,24 +1,15 @@
 <template>
-  <div class="footer">
+  <footer class="footer">
     <div class="links">
-      <a
-        href="https://pro.loacg.com/"
-        target="_blank"
-      >Pro 首页</a>
-      <a
-        href="https://github.com/sendya/ant-design-pro-vue"
-        target="_blank"
-      >
+      <a href="https://pro.loacg.com/" target="_blank">Pro 首页</a>
+      <a href="https://github.com/sendya/ant-design-pro-vue" target="_blank">
         <a-icon type="github" />
       </a>
       <a href="https://ant.design/">Ant Design</a>
       <a href="https://vue.ant.design/">Vue Antd</a>
     </div>
-    <div class="copyright">
-      Copyright
-      <a-icon type="copyright" /> 2018 <span>白鹭学园技术组出品</span>
-    </div>
-  </div>
+    <div class="copyright">Copyright<a-icon type="copyright" /> 2020 <span>哔哩哔哩动画矿业无限公司</span></div>
+  </footer>
 </template>
 
 <script>
