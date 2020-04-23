089ce08bb4264e5552cfd98d1c567a5752f3c24e
leiyalin <leiyalin@htbaobao.com>
2020-04-23 10:19:58 +0800

    🍻

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,7 +1,7 @@
 import { isIE } from '@/utils/util'
 
 // 判断环境不是 prod 或者 preview 是 true 时，加载 mock 服务
-if (process.env.NODE_ENV !== '') {
+if (process.env.NODE_ENV !== 'production') {
   if (isIE()) {
     console.error('ERROR: `mockjs` NOT SUPPORT `IE` PLEASE DO NOT USE IN `production` ENV.')
   }
