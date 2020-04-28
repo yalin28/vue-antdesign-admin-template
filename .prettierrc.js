542daf1c4452714179c34d94311868367deaa7e1
leiyalin <leiyalin@htbaobao.com>
2020-04-28 11:33:14 +0800

    ✨

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,6 @@ module.exports = {
   printWidth: 150, // 一行的字符数，如果超过会进行换行，默认为80
   tabWidth: 2, // 一个tab代表几个空格数，默认为2
   singleQuote: true, // 字符串是否使用单引号，默认为false，使用双引号
-  semi: false // 行尾部是否使用分号，默认为true
+  semi: false, // 行尾部是否使用分号，默认为true
+  endOfLine: 'auto', // fix Delete `␍`eslint(prettier/prettier)
 }
